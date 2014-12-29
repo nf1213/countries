0f1eaeb3e378ad96c4e40f8431fb131b8df255f4
Nicole <nfelch121394@gmail.com>
2014-12-29 17:10:21 -0500

    All user stories added

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -1,8 +1,13 @@
 require "rails_helper"
 
 feature "User visits the home page", %q(
+  As a user,
+  I would like to see all the countries,
+  So that I can find cities within a country
 
-
+  Acceptance Criteria
+    * I should see a map or list of all the countries
+    * I should be able to sign up and sign in
 ) do
 
   scenario "" do
