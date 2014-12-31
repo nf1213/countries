b0d7eed37629c8fd78bbacd6a4d21bf8483d040e
Allison <allisonmariebrowne@gmail.com>
2014-12-31 10:17:37 -0500

    fixed quotes in authentication_helper

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -2,18 +2,18 @@ module AuthenticationHelper
   def sign_in(user)
     visit new_user_session_path
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
 
-    click_button 'Log in'
+    click_button "Log in"
   end
 
   def sign_up(user)
     visit new_user_registration_path
-    fill_in 'User Name', with: user.username
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    fill_in 'Password confirmation', with: user.password
-    click_button 'Sign up'
+    fill_in "User Name", with: user.username
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password
+    click_button "Sign up"
   end
 end
