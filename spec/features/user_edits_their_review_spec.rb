ae03c7a0edfed8633ba2922f43a78ffb5bc02307
Nicole <nfelch121394@gmail.com>
2014-12-31 14:29:24 -0500

    Some crud tests

--- spec/features/user_edits_their_review_spec.rb
+++ spec/features/user_edits_their_review_spec.rb
@@ -7,11 +7,41 @@ feature "User edits their review", %q(
 
   Acceptance Criteria:
     * User can edit any of their ratings or their comment
-    * The review will be updated on all pages
+    * The review will be updated
 ) do
 
-  scenario "" do
+  before(:each) do
+    @city = FactoryGirl.create(:city_with_reviews)
+    @review = @city.reviews.first
+  end
 
+  scenario "Unauthenticated user tries to edit a review" do
+    visit edit_country_city_review_path(@city.country.id, @city.id, @review.id)
+
+    expect(page).to have_content "You must be signed in"
+  end
+
+  scenario "Unauthorized user tries to edit a review" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+
+    visit edit_country_city_review_path(@city.country.id, @city.id, @review.id)
+
+    expect(page).to have_content "You are not authorized to edit this review"
+  end
+
+  scenario "User edits their own review" do
+    user = @review.user
+    sign_in(user)
+    visit edit_country_city_review_path(@city.country.id, @city.id, @review.id)
+
+    select "5 star", from: "Food rating"
+    select "1 star", from: "Nightlife rating"
+    fill_in "Description", with: "An edited review"
+
+    expect(page).to have_content "Review succesfully updated"
+    expect(page).to have_content "An edited review"
+    expect(page).to have_content "Food rating: 5 star"
   end
 
 end
