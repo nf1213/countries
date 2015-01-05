d9bd220b808c57988a061c5f82f94d6212bc0b4f
Nicole <nfelch121394@gmail.com>
2015-01-05 11:28:11 -0500

    Vote validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,9 @@
 class Vote < ActiveRecord::Base
+  validates :review_id,
+    presence: true
+  validates :user_id,
+    presence: true
+  validates :value,
+    presence: true,
+    inclusion: {in: [-1, 1]}
 end
