6db2486935f8eaa8b23ddf6fa52e18d63c6967fb
pko104 <peterrko1004@gmail.com>
2015-01-05 17:57:15 -0500

    Added test for adding a profile photo and default profile photo

---
+++ spec/features/user_creates_an_avatar_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'user signs in', %Q{
+  As a signed up user
+  I want to sign in
+  So that I can see my profile photo and change it
+} do
+
+  before(:each) do
+    @user = FactoryGirl.build(:user)
+    sign_up(@user)
+  end
+
+  scenario "User has default profile photo" do
+    visit root_path
+
+    click_on "Edit User Profile"
+
+    click_on "Edit Profile"
+
+    expect(page).to have_selector("img[src$='cat.png']")
+  end
+
+  scenario "User changes profile photo" do
+
+    visit root_path
+
+    click_on "Edit User Profile"
+
+    attach_file 'Add a Photo', "./app/assets/images/dog.png"
+
+    click_on "Edit Profile"
+
+    expect(page).to have_selector("img[src$='profilephoto.jpg']")
+    #expect(page).to have_selector("img[src$='dog.png']")
+  end
+
+end
