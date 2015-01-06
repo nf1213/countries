f8cf6aeed83013a096f835efb4be695977126592
pko104 <peterrko1004@gmail.com>
2015-01-06 15:12:57 -0500

    added or statements

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
+
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID']|| '',
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']|| ''
@@ -7,4 +8,5 @@ CarrierWave.configure do |config|
   config.fog_directory  = ENV['S3_BUCKET']|| ''
   config.fog_public     = false
   config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"}
+
 end
