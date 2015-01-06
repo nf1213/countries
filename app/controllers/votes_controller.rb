2bafc7074b66ab47100ac91d2c8b28a7a8db6a9f
pko104 <peterrko1004@gmail.com>
2015-01-06 18:28:32 -0500

    save branch

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,18 +4,21 @@ class VotesController < ApplicationController
   def create
     review = Review.find(params[:vote][:review_id])
     city = review.city
+    @user = review.user
     country = city.country
     vote = Vote.new(vote_params)
     vote.user_id = current_user.id
     notice = ""
     if vote.save
       notice = "You have voted!"
+      VoteMailer.welcome_email(@user).deliver_now
     else
       existing = review.votes.find_by(review_id: review.id, user_id: current_user.id)
       if existing
         if existing.value != vote.value
           existing.update(value: vote.value)
           notice = "You have voted!"
+          VoteMailer.welcome_email(@user).deliver_now
         else
           notice = "You already voted!"
         end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,15 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
