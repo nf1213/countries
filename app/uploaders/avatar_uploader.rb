ddc7d3cc1812561485a3b21f3edab9a03632af52
pko104 <peterrko1004@gmail.com>
2015-01-06 17:55:25 -0500

    fixed avatar uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,10 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  if Rails.env.production? || Rails.env.development?
-    storage :fog
-  else
-    storage :file
-  end
 
   def extension_white_list
     %w(jpg jpeg gif png)
