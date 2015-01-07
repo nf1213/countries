a5a19a8c0bf4f1912dac1112c6e322204ef5da56
Allison <allisonmariebrowne@gmail.com>
2015-01-07 08:20:05 -0500

    added admin module to admin namespace cities controller

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,10 +1,11 @@
-class Admin::CitiesController < ApplicationController
-  def index
-    @cities = City.all
-    if !current_user.admin
-      flash[:notice] = "Admins Only"
-      redirect_to root_path
+module Admin
+  class CitiesController < ApplicationController
+    def index
+      @cities = City.all
+      if !current_user.admin
+        flash[:notice] = "Admins Only"
+        redirect_to root_path
+      end
     end
   end
-  
 end
