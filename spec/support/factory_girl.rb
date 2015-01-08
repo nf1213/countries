b196eee43501c43f5ebba3ab316e52948c26677e
Nicole <nfelch121394@gmail.com>
2015-01-08 13:31:27 -0500

    Unused factory

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -19,6 +19,7 @@ FactoryGirl.define do
     cost_rating 4
     weather_rating 5
     culture_rating 3
+    overall_rating 0
     sequence(:description) { |n| "#{n}the house with the place of the city." }
     user
     city
@@ -28,17 +29,6 @@ FactoryGirl.define do
     sequence(:name) { |n| "Space Boston#{n}" }
     country
     rating 0
-
-    factory :city_with_reviews do
-
-      transient do
-        reviews_count 5
-      end
-
-      after(:create) do |city, evaluator|
-        create_list(:review, evaluator.reviews_count, city: city)
-      end
-    end
   end
 
   factory :country do
